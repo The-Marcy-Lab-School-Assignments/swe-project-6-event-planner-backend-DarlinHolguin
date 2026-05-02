86d03d1af1e0221e6a4e53f90504b0ee3737d943
[DarlinHolguin] <[darlinholguin123@gmail.com]>
2026-05-01 23:10:52 -0400

    fix: renamed controllers folder and auth session key

--- server/middleware/checkAuthentication.js
+++ server/middleware/checkAuthentication.js
@@ -1,5 +1,5 @@
 const checkAuthentication = (req, res, next) => {
-    if (!req.session.userId) {
+    if (!req.session.user_id) {
         return res.status(401).send({ message: 'You must be logged in.' });
     }
     next();
